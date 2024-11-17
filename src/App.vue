d9db0ebdbc114f940d94ef007641fce2041810f1
jasond <jasondansie@gmail.com>
2024-11-17 12:38:29 +0200

    adds branch from old repo

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,60 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import menuBar from './components/menuBar.vue';
+import DashBoard from './components/dashBoard.vue';
+import AboutPage from './components/aboutPage.vue';
+import CasesPage from './components/casesPage.vue';
+import AgentPage from './components/agentsPage.vue';
+
+import { ref } from 'vue';
+
+
+// Register your components
+const componentsMap = {
+  Home: DashBoard,
+  Cases: CasesPage,
+  Agents: AgentPage,
+  About: AboutPage,
+};
+
+// Define a reactive variable for the active component
+const activeComponent = ref('Home');
+
+// Function to update the active component
+function updateComponent(componentName) {
+  activeComponent.value = componentName;
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="layout">
+    <aside class="menu-bar">
+      <menuBar @menu-item-click="updateComponent" />
+    </aside>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="content">
+      <!-- Dynamically load the component -->
+      <component :is="componentsMap[activeComponent]" />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.layout {
+  display: flex;
+  height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.menu-bar {
+  width: 200px;
+  background-color: white;
+  color: white;
+  padding: 10px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.content {
+  flex-grow: 1;
+  padding: 20px;
 }
 </style>
